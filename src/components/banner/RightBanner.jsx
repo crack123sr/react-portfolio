af58f6e89b2a3f32112652e9bae8ae4e915d34e0
Priyanshu Bhardwaj <pb12asofficial@gmail.com>
2025-01-15 03:39:25 +0530

    changed bannerImg

--- src/components/banner/RightBanner.jsx
+++ src/components/banner/RightBanner.jsx
@@ -5,11 +5,11 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[414px] h-[680px] lgl:w-[414px] lgl:h-[680px] z-10 bg-cover"
+        className="w-[443px] h-[580px] lgl:w-[433px] lgl:h-[580px] z-10 bg-cover"
         src={bannerImg}
         alt="bannerImg"
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[414px] lgl:h-[680px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[443px] lgl:h-[580px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
     </div>
   );
 }
